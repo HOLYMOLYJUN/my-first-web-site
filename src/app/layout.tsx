0b4ed5d80267c76e119d5a81ed94704ebb98a3d6
leesangjun <lsj50316@gmail.com>
2024-01-27 01:50:30 +0900

    테스트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
+      <body>{children} </body>
     </html>
   );
 }
